df3c4ba5f0398e9f7a686b417012a6cf8b90a26e
htcstudent <htcstudent@BH255WD10>
2017-02-23 15:45:45 -0600

    Add project files.

---
+++ Family/Family/FamilyPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Family
+{
+    class FamilyPage : ContentPage
+    {
+        public FamilyPage()
+        {
+            Title = "My Family";
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = Color.FromHex("#000000"),
+                Children = {
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Kyle (Me)",
+                        FontAttributes = FontAttributes.Bold,
+                        TextColor = Color.FromHex("#FFFFFF")
+                    },
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Ashley (Girlfriend)",
+                        FontAttributes = FontAttributes.Bold,
+                        TextColor = Color.FromHex("#FFFFFF")
+
+                    },
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Joey (Dog)",
+                        FontAttributes = FontAttributes.Bold,
+                        TextColor = Color.FromHex("#FFFFFF")
+
+                },
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Oscar (Cat)",
+                        FontAttributes = FontAttributes.Bold,
+                        TextColor = Color.FromHex("#FFFFFF")
+
+                },
+                    new Label {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Knight (Rabbit)",
+                        FontAttributes = FontAttributes.Bold,
+                        TextColor = Color.FromHex("#FFFFFF")
+
+                }
+             }
+          };
+       }
+   }
+}
